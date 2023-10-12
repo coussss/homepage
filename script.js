e529c2fd97a916051d9c59ba30843bbb34c86f03
coussss <adamjbox@gmail.com>
2023-10-12 18:45:05 +0100

    new repo made

---
+++ script.js
@@ -0,0 +1,32 @@
+function cButton(){
+    window.open("https://en.wikipedia.org/wiki/C_(programming_language)",'_blank');
+}
+function pythonButton(){
+    window.open("https://en.wikipedia.org/wiki/Python_(programming_language)",'_blank');
+}
+function htmlButton(){
+    window.open("https://en.wikipedia.org/wiki/HTML_(programming_language)",'_blank');
+}
+function cssButton(){
+    window.open("https://en.wikipedia.org/wiki/CSS",'_blank');
+}
+function jsButton(){
+    window.open("https://en.wikipedia.org/wiki/Javascript",'_blank');
+}
+
+
+document.getElementById("instagram").onclick = function(){
+    window.open("https://www.instagram.com/_adamboxall/");
+}
+document.getElementById("github").onclick = function(){
+    window.open("https://github.com/coussss");
+}
+document.getElementById("tiktok").onclick = function(){
+    window.open("https://www.tiktok.com/@couscouskingpin");
+}
+document.getElementById("reddit").onclick = function(){
+    window.open("https://www.reddit.com/user/adam_boxall");
+}
+document.getElementById("discord").onclick = function(){
+    window.open("https://discordapp.com/users/220945317003132929");
+}
